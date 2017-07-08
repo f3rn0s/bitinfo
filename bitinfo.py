db2a42c1f6654e5d02f87e213bb50141c7557665
Teal <tealkine2@gmail.com>
2017-07-08 19:52:37 +1000

    Revert "Update bitinfo.py"

--- bitinfo.py
+++ bitinfo.py
@@ -8,8 +8,7 @@ addr = input().rstrip()
 #addr = ""
 
 #Currency Code
-print("Enter Currency: ", end='')
-currencycode = input().rstrip()
+currencycode = 'AUD'
 
 #Sets up the websites to grab
 r1 = requests.get('https://blockchain.info/en/address/' +
@@ -20,25 +19,20 @@ r2  = requests.get('http://api.coindesk.com/v1/bpi/currentprice/' +
 #Stores the requests json
 try:
     bitaddrinfo = r1.json()
+    bitconvinfo = r2.json()
 except:
     sys.exit("Invalid Address")
 
-#Grabs the exchange rate
-try:
-    bitconvinfo = r2.json()
-    rate = bitconvinfo['bpi'][currencycode]['rate'][2:]
-    balanceconv = round((bitaddrinfo['final_balance'] * 0.00000001) * float(rate), 2)
-except:
-    print("Error obtaining conversion rates...")
-    balanceconv = "???"
-
+#Grabs the exchange rate for AUD
+rate = bitconvinfo['bpi']['AUD']['rate'][2:]
+balanceconv = round((bitaddrinfo['final_balance'] * 0.00000001) * float(rate), 2)
 balance = round(bitaddrinfo['final_balance'] * 0.00000001, 4)
 
 #Prints The Account Information
 print('Account information')
 print('Address:         ' + str(bitaddrinfo['address']))
 print('Balance:         ' + str(balance))
-print('Balance ('+currencycode+'):   ' + str(balanceconv) + '\n')
+print('Balance (AUD):   ' + str(balanceconv) + '\n')
 
 print('Transaction History')
 print('Total Recieved:  ' + str(float(bitaddrinfo['total_received'] / 100000000)))
